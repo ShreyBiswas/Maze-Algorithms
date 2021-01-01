9e8a796e34fd94eab89e02852189fea28e7de918
StreakSharn <74095821+StreakSharn@users.noreply.github.com>
2021-01-01 18:44:15 +0000

    Cleaning

--- BFS.py
+++ BFS.py
@@ -54,63 +54,6 @@ def bf_search(grid, pathDict):
         current = newCell[0]
         time.sleep(0.05)
 
-    
-    
-    
-    
-    
-    
-    '''
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    current = start
-    frontier.append(current)
-    parentDict[current] = [current]
-    newCell = current
-    #* Search until destination is found
-    while len(frontier)>0:
-
-        #time.sleep(0.001)
-        newCell = frontier.pop(0)
-
-        possibleFrontier = generator.find_frontier(cellWidth,newCell,grid,visited)
-        possibleFrontier = [node[0] for node in possibleFrontier]
-        
-        for node in possibleFrontier:
-            try:
-                parentDict[newCell].append[node]
-            except:
-                parentDict[newCell] = [node]
-            frontier.append(node)
-        
-        frontier = list(dict.fromkeys(frontier))
-
-
-        visited.append(current)
-        highlight(current,newCell,cellWidth,'search')
-        current = newCell
-       '''
-
-    #TODO: MAKE SURE TARGET CELL IS SCANNED, STOPS ABOVE. ADD REVERSING DICTIONARY FOR SOLUTION
-'''
-    print(list(parentDict.keys())[-5:])
-    print(list(parentDict.values())[-5:])
-
-    #* work backwords for optimal solution
-    while True:
-        newCell,current = parentDict[newCell],newCell
-        highlight(current,newCell,cellWidth,'solution')
-'''
 def highlight(current,newCell,cellWidth,situ):
     if situ == 'search':
         pygame.draw.rect(generator.window, generator.RED, (current[0]+1, current[1]+1, cellWidth-1, cellWidth-1),0)
